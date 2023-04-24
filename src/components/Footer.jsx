6a711b297484ca0add83ec7ba694fa890196dfd8
Nico <juareznico61@gmail.com>
2023-04-24 19:14:48 -0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/img/logoNJ.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from '../assets/img/github-removebg-preview.png';
 
 export const Footer = () => {
   return (
@@ -16,6 +17,7 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/nicolas-juarez95/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/Nicojuarez95" target="_blank"><img src={navIcon4} alt="" /></a>
               <a href="https://www.facebook.com/NicoJuarezOk" target="_blank"><img src={navIcon2} alt="Icon" /></a>
               <a href="https://www.instagram.com/nicojuarez95/" target="_blank"><img src={navIcon3} alt="Icon" /></a>
             </div>
